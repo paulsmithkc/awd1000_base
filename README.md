b23ad23e91107b3453fae6dc5a0fae33bd37039a
Paul Smith <prsmith@ranken.edu>
2021-06-24 11:18:35 -0500

    adding lots of configuration to the template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AWD1000
+# Template repo for AWD1000 projects
+
+Put all your code inside the src folder
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// create server
+const app = express();
+
+// routes
+app.use('/', express.static('src', { index: 'index.html' }));
+
+// start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`); // eslint-disable-line no-console
+});
